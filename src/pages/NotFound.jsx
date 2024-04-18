b928b22b9c2f3cf15f3108507b0c8d22b05d5ecf
Ulternae <ulternae@gmail.com>
2024-04-18 17:10:45 -0500

    Add styles of page notFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import { Layout } from "./Layout";
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <p>NotFound</p>
-    </div>
-  )
-}
+    <Layout>
+      <div className="grid place-content-center md:place-content-end ">
+        <div className="w-[290px]">
+          <h1 className="text-2xl mb-4 md:mb-0 font-bold text-center md:text-start">
+            This page not was found
+          </h1>
+          <button
+            className="mt-6 w-48 h-10 bg-neutral-400 rounded-lg border-2 border-neutral-700"
+            onClick={() => navigate("/")}
+          >
+            Go to home
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export { NotFound }
+export { NotFound };
